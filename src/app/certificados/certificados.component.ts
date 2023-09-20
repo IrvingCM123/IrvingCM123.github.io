45c77049eac07d9413cc016c00382761092dc79f
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-09-20 03:44:12 -0600

    cambios

--- src/app/certificados/certificados.component.ts
+++ src/app/certificados/certificados.component.ts
@@ -41,14 +41,32 @@ export class CertificadosComponent implements OnInit {
       Nombre: "Curso de Python",
       Descripcion: "Curso realizado en Kaggle",
       Experiencia: "2 dias",
-      Imagen: "../../assets/Certificado.png"
+      Imagen: "../../assets/Certificado.png",
+      link: "https://firebasestorage.googleapis.com/v0/b/pruebas-abcf6.appspot.com/o/irving%2FIrving%20Rafael%20Conde%20Marin%20-%20Python.png?alt=media&token=86aa2e30-411e-40f1-afeb-beb57e72d24f"
     }
 
     let Certificado6 = {
       Nombre: "Curso de Pandas",
       Descripcion: "Curso realizado en Kaggle",
       Experiencia: "2 dias",
-      Imagen: "../../assets/Irving Rafael Conde Marin - Pandas.png"
+      Imagen: "../../assets/Irving Rafael Conde Marin - Pandas.png",
+      link: "https://firebasestorage.googleapis.com/v0/b/pruebas-abcf6.appspot.com/o/irving%2FIrving%20Rafael%20Conde%20Marin%20-%20Pandas.png?alt=media&token=124fba51-b9d5-4976-8ec4-ffc86382ae08"
+    }
+
+    let Certificado7 = {
+      Nombre: "Curso de intro maching lerning",
+      Descripcion: "Curso realizado en Kaggle",
+      Experiencia: "2 dias",
+      Imagen: "../../assets/Intro to Machine Learning.png",
+      link: "https://firebasestorage.googleapis.com/v0/b/pruebas-abcf6.appspot.com/o/irving%2FIntro%20to%20Machine%20Learning.png?alt=media&token=dc0ed52a-922a-46b4-9883-48cd0e007d04"
+    }
+
+    let Certificado8 = {
+      Nombre: "Curso de intermediate maching lerning",
+      Descripcion: "Curso realizado en Kaggle",
+      Experiencia: "2 dias",
+      Imagen: "../../assets/intermediate Machine Learning.png",
+      link: "https://firebasestorage.googleapis.com/v0/b/pruebas-abcf6.appspot.com/o/irving%2Fintermediate%20Machine%20Learning.png?alt=media&token=99bd424a-527b-4764-ac13-d77a40f6d53e"
     }
 
 
@@ -57,7 +75,9 @@ export class CertificadosComponent implements OnInit {
     this.Certificados.push(Certificado3);
     this.Certificados.push(Certificado4);
     this.Certificados.push(Certificado5);
-    this.Certificados.push(Certificado6)
+    this.Certificados.push(Certificado6);
+    this.Certificados.push(Certificado7);
+    this.Certificados.push(Certificado8);
   }
 
 }
